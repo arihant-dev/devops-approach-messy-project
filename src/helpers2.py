7e13b00a35457c0e1ca150a33ee17d8ea99c3fb7
musajoshua <gidijosh@gmail.com>
2026-05-13 15:47:55 +0200

    feat: add dates and eggplant to data array

--- src/helpers2.py
+++ src/helpers2.py
@@ -1,6 +1,6 @@
 import random
 
 def getData():
-    data = ["apple", "banana", "cherry"]
+    data = ["apple", "banana", "cherry", "dates", "eggplant"]
     idx = random.randint(0, len(data)-1)
     return data[idx]
